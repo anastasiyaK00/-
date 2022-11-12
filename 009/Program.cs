236279dbe5ca960bfdae68fd59b50816fc3acd62
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-12 19:51:51 +0300

    add 009

---
+++ 009/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывести на экран четные числа от 1 до N
+System.Console.WriteLine("Введите число ");
+int n=Convert.ToInt32(Console.ReadLine());
+int i=2;
+while (i<=n)
+{
+    System.Console.Write($"{i} ");
+    i=i+2;
+}
+
+System.Console.WriteLine();
+for(int j=1;j<=n;j=j+1)
+{
+    if(j%2==0)
+    {
+        System.Console.Write($"{j} ");
+    }
+}
