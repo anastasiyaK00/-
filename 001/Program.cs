7733c891e3e5f5a897f38900f1bf6a835f1069c4
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-12 19:47:49 +0300

    add 001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,35 +1,21 @@
-﻿// С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
-/* Правила написания
-||Полное условие:
-if()
-{
-    //True
-}
-else
-{
-    //false
-}
-||Неполное условие: 
-if()
-{
-    true
-}
+﻿/*
+int a=0, b=0, c=0;
+float f=20.4f;
+double d=3.14;
+string s="hello";
+char c='a';
+bool flag=true;
+int переменная1=0;
 */
+//С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
+#region Мой код
+Console.WriteLine("Hello");
+#endregion
 
-
-System.Console.WriteLine("Введите число a:");
-double a=Convert.ToDouble(Console.ReadLine());
-System.Console.WriteLine("Введите число b:");
-double b=Convert.ToDouble(Console.ReadLine());
-if (a*a==b) 
-{
-    System.Console.WriteLine($"{b}является квадратом {a}");
-}
-if (b*b==a) 
-{
-    System.Console.WriteLine($"{a}является квадратом {b}");
-}
-else
-{
-   System.Console.WriteLine("Не является"); 
-}
+int a;
+a=10;
+System.Console.WriteLine("введите число:");
+string? s=Console.ReadLine();// string?(строчка). Здесь присваивается строчка, но не значение 
+a=Convert.ToInt32(s);// Комманда ToInt32 позволяет строчку преобразовать в значение!!т.о мы получаем значение.
+int b=a*a;
+System.Console.WriteLine($"{a}^2={b}");
