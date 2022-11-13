ee165ee78c2f65ef385042ef2f712cd7183e29e5
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-13 19:48:23 +0300

    add 008

---
+++ 008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести на экран числа от -N до N
+System.Console.WriteLine("Введите число ");
+int n=Convert.ToInt32(Console.ReadLine());
+int i=-n;
+while (i<=n)
+{
+    System.Console.Write($"{i} ");
+    i=i+1;
+}
+
+//Но если мы вводим -N, то он не будет выводить....
+// предполагаю, что нужно добавить еще цикл, но пока не понимаю как.
