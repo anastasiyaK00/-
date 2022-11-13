c6ff746ccfec216a9964030329ead90b4e67dfc5
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-13 19:31:36 +0300

    add 007

---
+++ 007/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным.
+
+System.Console.WriteLine("Введите число ");
+int K=Convert.ToInt32(Console.ReadLine());
+
+
+if (K%2==0)
+{
+  System.Console.WriteLine($"{K}-число четное");  
+}
+else System.Console.WriteLine($"{K}-число нечетное"); 
