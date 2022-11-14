104311f126a9bb7eb22621ade688cac336855ef8
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-14 19:49:09 +0300

    add 010

---
+++ 010/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+System.Console.WriteLine("Введите число ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+int s;
+s=n%10;
+System.Console.WriteLine($"{s}");
