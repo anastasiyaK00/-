af9499e7b4f8504ba532c5b2d9d79077b8a97656
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-17 21:24:55 +0300

    add 012

--- 012/Program.cs
+++ 012/Program.cs
@@ -1,8 +1,13 @@
 ﻿/* С клавиатуры вводится целое число из диапазона  [10, 99]. 
 Показать наибольшую цифру числа
 */
+int n;
+do
+{
 System.Console.WriteLine("Введите число от 10 до 99 ");
-int n=Convert.ToInt32(Console.ReadLine());
+n=Convert.ToInt32(Console.ReadLine());
+}
+while (n<10 || n>99);
 
 int s;
 s=n%10;
