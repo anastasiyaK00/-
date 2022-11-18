2f05aa6756c1e8c6602b6b24da27a0f39ac42708
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-18 23:28:32 +0300

    016

---
+++ 016/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int n=Convert.ToInt32(Console.ReadLine());
+bool d1=n%7==0;
+bool d2=n%23==0;
+if (d1 && d2) System.Console.WriteLine("Yeees");
+else System.Console.WriteLine("Noooo");
+
+//if (n%7==0 && n%23==0)System.Console.WriteLine("Yeees");
+//else System.Console.WriteLine("Noooo");
