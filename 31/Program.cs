aab17a110d8b7ec20ab84ccf4464c8a4384b130f
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-20 21:05:34 +0300

    31

--- 31/Program.cs
+++ 31/Program.cs
@@ -9,6 +9,7 @@
 //new Random().Next(1,100); Так показывали на лекции.
 */
 /*
+
 int r = 10;
 double t = r; //неявное приведение типов
 double x = 23;
@@ -16,6 +17,22 @@ double x = 23;
 int o = (int)x;//Явное приведение типов 
 */
 
+bool Test(int a) //Подпрограмма, проверяющая подходит или не подходит число под условия задачи.
+{
+    //if(a%2==0) return true; else return false;
+    return a * a * a % 2 == 0;
+}
+
+Random random = new Random();
+for (int i = 0; i < 5; i++)
+{
+    int a = random.Next(1, 100);
+    int b = (int)Math.Pow(a, 3);
+    if (Test(b))
+        System.Console.WriteLine($"Случайное число={a}, Куб этого числа={b}");
+}
+
+/*Программа 1(не с 10 числами)
 Random random = new Random();
 for (int i = 0; i < 5; i++)
 {
@@ -24,7 +41,7 @@ for (int i = 0; i < 5; i++)
     if (b % 10 % 2 == 0)
         System.Console.WriteLine($"Случайное число={a}, Куб этого числа={b}");
 }
-
+*/
 /*
 Random random = new Random();
 for (int i = 0; i < 5; i++)
